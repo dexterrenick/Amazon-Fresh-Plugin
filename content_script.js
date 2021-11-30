ef41537ab48207a3dafd89705ca5d9183dad38e1
dexterrenick <dexter.renick@gmail.com>
2021-11-29 21:56:06 -0500

    adding bread

--- content_script.js
+++ content_script.js
@@ -1,8 +1,3 @@
-
-const items = document.querySelectorAll('.a-size-base.a-color-base.a-text-normal');
-const prices = document.querySelectorAll('.a-row.a-size-base.a-color-base');
-
-
 const fp = [
  {
    "food": "Beef (beef herd)",
@@ -135,7 +130,14 @@ const fp = [
  {"food": "tea",
   "emissions per kg": "1.9 kg"
   }
+  ,
+  {"food": "bread loaf",
+   "emissions per kg": "1.5 kg"
+   }
 ]
+
+const items = document.querySelectorAll('.a-size-base-plus.a-color-base.a-text-normal');
+const prices = document.querySelectorAll('.a-row.a-size-base.a-color-base');
 
 itemList = []
 priceList = []
@@ -212,17 +214,21 @@ function getWeight(foodTitle) {
 }
 
 function calculateEmissions(foodItem) {
-  for (let item in fp) {
-    let foodWords = fp[item]["food"].split(" ")
-    for (let word in foodWords) {
-      if (foodItem.toLowerCase().includes(foodWords[word].toLowerCase())) {
-        if (getWeight(foodItem) != 0) {
-          return Math.round(getWeight(foodItem) * parseFloat(fp[item]["emissions per kg"])*100)/100
+  try {
+    for (let item in fp) {
+      let foodWords = fp[item]["food"].split(" ");
+      for (let word in foodWords) {
+        if (foodItem.toLowerCase().includes(foodWords[word].toLowerCase())) {
+          if (getWeight(foodItem) != 0) {
+            return Math.round(getWeight(foodItem) * parseFloat(fp[item]["emissions per kg"])*100)/100
+          }
         }
       }
     }
+    return 'unknown';
+  } catch (e) {
+    return 'unknown';
   }
-  return 'unknown';
 }
 
 
@@ -273,30 +279,29 @@ for(let item = 0; item < cartItems.length-1; item++) {
   let e = calculateEmissions(cartItems[item].firstChild.innerHTML);
   if (e != 'unknown') {
     totalEmissions += calculateEmissions(cartItems[item].firstChild.innerHTML);
-    console.log(totalEmissions);
   }
 }
 
 
 // Clone Carousel
-try {
-  const cartTitle = document.querySelectorAll('.a-section.a-padding-small');
-  console.log(cartTitle);
-  let totalEmissionsText = document.createElement("div");
-  let wr = document.createElement("div");
-  wr.setAttribute("style", "display: flex");
-  totalEmissionsText.append('Estimate of Cart Total Emissions: ');
-  totalEmissionsText.append(totalEmissions);
-
-  let subscript = document.createElement("sub");
-  subscript.append("2");
-
-  totalEmissionsText.append(`kg of CO`);
-  totalEmissionsText.append(subscript);
-  wr.append(totalEmissionsText);
-
-  totalEmissionsText.setAttribute("style", "font-size: 24px; font-weight: bold; padding-left: 40px; padding-top: 20px;")
-  cartTitle[0].parentNode.insertBefore(wr, cartTitle[0]);
-}
-catch (exception_var) {
-}
+// try {
+//   const cartTitle = document.querySelectorAll('.a-section.a-padding-small');
+//   console.log(cartTitle);
+//   let totalEmissionsText = document.createElement("div");
+//   let wr = document.createElement("div");
+//   wr.setAttribute("style", "display: flex");
+//   totalEmissionsText.append('Estimate of Cart Total Emissions: ');
+//   totalEmissionsText.append(totalEmissions);
+//
+//   let subscript = document.createElement("sub");
+//   subscript.append("2");
+//
+//   totalEmissionsText.append(`kg of CO`);
+//   totalEmissionsText.append(subscript);
+//   wr.append(totalEmissionsText);
+//
+//   totalEmissionsText.setAttribute("style", "font-size: 24px; font-weight: bold; padding-left: 40px; padding-top: 20px;")
+//   cartTitle[0].parentNode.insertBefore(wr, cartTitle[0]);
+// }
+// catch (exception_var) {
+// }
